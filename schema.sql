ad1bd23984cfc682d74b79e1d9b880fea2cbc0ca
Angel <skelliebunnie@gmail.com>
2021-01-25 09:57:42 -0800

    updated schema.sql to include db creation/use statements.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
 CREATE TABLE departments (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(30) NOT NULL,
